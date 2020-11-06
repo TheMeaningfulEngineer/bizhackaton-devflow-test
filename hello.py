e44ea35eb5e05fa2f4b6488c78fa4b752d0b6a1f
Alan Martinovic <alan.martinovic@gmail.com>
2020-11-06 14:50:57 +0100

    WIP

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
+import time
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
