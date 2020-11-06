b260cd91f9ba1c0e875dd700977230a1d1ea1741
Alan Martinovic <alan.martinovic@gmail.com>
2020-11-06 14:32:57 +0100

    Revert "frontend setup"
    This reverts commit d829ca982ae280ecf4a52f7b9882f2555b039ca3.

--- hello.py
+++ hello.py
@@ -1,12 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-import time
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
