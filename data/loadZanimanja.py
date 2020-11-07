e9a816963d94e399e9352ca7cc3ce746bbe3162e
jjurcevic9@gmail.com <josko-heh>
2020-11-07 21:32:26 +0100

    WIP

--- data/loadZanimanja.py
+++ data/loadZanimanja.py
@@ -1,6 +1,9 @@
 import pandas
 
-mainPath = "D:\\preuzimanje\\Hackaton\\bizhackaton-devflow-test-main\\bizhackaton-devflow-test-main\\data\\"
+
+#mainPath = "D:\\preuzimanje\\Hackaton\\bizhackaton-devflow-test-main\\bizhackaton-devflow-test-main\\data\\"
+mainPath = ""
+
 path = mainPath + "nkd2007.xlsx"
 nkd2007Df = pandas.read_excel(path)
 
@@ -10,10 +13,6 @@ povlastenaDf = pandas.read_excel(path)
 path = mainPath + "PravilnikVezana.xlsx"
 vezanaDf = pandas.read_excel(path)
 
-#print(nkd2007Df.head(10))
-#print(povlastenaDf.head(10))
-#print(vezanaDf.head(10))
-
 
 nkd2007Df = nkd2007Df.dropna(subset=['Razred'])
 del nkd2007Df["Po-druč-je"]
@@ -21,30 +20,36 @@ del nkd2007Df["Odje-ljak"]
 del nkd2007Df["Sku-pina"]
 
 # Za dev potrebe samo da prikazuje dio
-mali_nkd = nkd2007Df.iloc[400:420]
+# vise ne koristimo mali_nkd, vidi return funkcije fetch_nkd
+#mali_nkd = nkd2007Df.iloc[400:420]
 
-
-def search_df_by_djelatnost_string(nkd_list, search_string):
-    regex = f".*{search_string}.*"
-    match_series = nkd_list.Naziv.str.contains(regex, case=False)
-    return nkd_list[match_series]
 
 
 def df_to_json(nkd_list, search_string):
+
+    def search_df_by_djelatnost_string(nkd_list, search_string):
+        '''Parameter nkd_list : DataFrame
+           Returns DataFrame'''
+        regex = f".*{search_string}.*"
+        match_series = nkd_list.Naziv.str.contains(regex, case=False)
+        return nkd_list[match_series]
+
     resultDf = search_df_by_djelatnost_string(nkd_list, search_string)
     resultJson = resultDf.to_json(orient="records")
     return resultJson
 
 
-# TODO: zavrsiti
-def fetch_nkd()
-    pass
-    # Vraca nkd dataframe i parsira ga svaki pu. Boze moj :)
+def fetch_nkd():
+    # funkcija mijenja globalnu varijablu nkd koja mijenja bazu; 
+    # kasnije sve to zamijeniti bazom
+    # Vraca nkd dataframe koji se parsira ga svaki put. Boze moj :)
+    return nkd2007Df #vratio puni nkd, a ne mali_nkd
+    
 
 
 
-# Primjer
-resultDf = search_df_by_djelatnost_string(mali_nkd, "prij")
-resultJson = resultDf.to_json(orient="records")
-parsed = json.loads(resultJson)
-print(json.dumps(parsed, indent=4))
+# Testiranje
+#import json
+#resultJson = df_to_json(mali_nkd, "prij")
+#parsed = json.loads(resultJson)
+#print(json.dumps(parsed, indent=4))
