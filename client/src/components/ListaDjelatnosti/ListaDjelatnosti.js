22f43fee2844c981884c0dd84aee47e5b2c84d1c
dorkele <dora_tominic@hotmail.com>
2020-11-07 21:38:27 +0100

    client

--- client/src/components/ListaDjelatnosti/ListaDjelatnosti.js
+++ client/src/components/ListaDjelatnosti/ListaDjelatnosti.js
@@ -7,11 +7,33 @@ export default function ListaDjelatnosti({ result }) {
     return (
         <div className={styles.container}>
             {result.map((item) => {
+                let style = {};
+                let style2 = {};
+                console.log(item.ime);
+                if (item.vezanost === "slobodna") {
+                    style2 = { borderColor: "#4A8FE7" };
+                    style = {
+                        backgroundColor: "#4A8FE7",
+                    };
+                } else if (item.vezanost === "vezana") {
+                    style = { backgroundColor: "#541388" };
+                    style2 = { borderColor: "#541388" };
+                } else {
+                    style = { backgroundColor: "#F45B69" };
+                    style2 = { borderColor: "#F45B69" };
+                }
+                console.log(style);
                 return (
-                    <div key={item.sifra} className={styles.lista}>
-                        <span className={styles.sifra}>{item.sifra}</span>
-                        <span className={styles.ime}>{item.ime}</span>
-                        <span className={styles.vezanost}>{item.vezanost}</span>
+                    <div
+                        key={item.sifra}
+                        className={styles.lista}
+                        style={style2}
+                    >
+                        <div className={styles.sifra}>{item.sifra}</div>
+                        <div className={styles.ime}>{item.ime}</div>
+                        <div className={styles.vezanost} style={style}>
+                            {item.vezanost}
+                        </div>
                     </div>
                 );
             })}
--- hello.py
+++ hello.py
@@ -14,8 +14,35 @@ def get_current_time():
 
 @app.route('/activities')
 def get_activities():
-    return {
+    return {   
+    1: {
         "sifra": "10.11",
         "ime": "Prerada i konzerviranje mesa",
         "vezanost": "vezana"
+    },
+    2: {
+        "sifra": "10.51.01",
+        "ime": "Prerada mlijeka i proizvodnja sira",
+        "vezanost": "vezana"
+    },
+    3: {
+        "sifra": "10.61.01",
+        "ime": "Mljevenje žitarica: proizvodnja brašna, drobine, prekrupe ili kuglica od pšenice, raži, zobi, kukuruza i drugih žitarica",
+        "vezanost": "vezana"
+    },
+    4: {
+        "sifra": "10.71",
+        "ime": "Proizvodnja kruha; proizvodnja svježih peciva i sličnih proizvoda te kolača",
+        "vezanost": "povlaštena"
+    },
+    5: {
+        "sifra": "95.29",
+        "ime": "Popravak knjiga",
+        "vezanost": "slobodna"
+    },
+    6: {
+        "sifra": "18.14",
+        "ime": "Izdavanje knjiga",
+        "vezanost": "slobodna"
     }
+}
